980cc4f081751c48bcf49255d4460a2b37e6d2cf
Jon Galloway <jongalloway@gmail.com>
2015-06-16 10:11:23 +0200

    Added EF lab code

---
+++ Labs/EF/End/Models/Identity/IdentityExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Framework.DependencyInjection;
+
+namespace UnicornStore.AspNet.Models.Identity
+{
+    public static class IdentityExtensions
+    {
+        public static void EnsureRolesCreated(this IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.GetService<ApplicationDbContext>();
+            if (context.AllMigrationsApplied())
+            {
+                var roleManager = app.ApplicationServices.GetService<RoleManager<IdentityRole>>();
+                foreach (var role in Roles.All)
+                {
+                    if (!roleManager.RoleExistsAsync(role.ToUpper()).Result)
+                    {
+                        roleManager.CreateAsync(new IdentityRole { Name = role });
+                    }
+                }
+            }
+        }
+    }
+}
