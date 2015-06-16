526100142bc5bd71b57ed737f0f2914ce05eeb35
Jon Galloway <jongalloway@gmail.com>
2015-06-16 14:40:09 +0200

    Added Begin and End states for SPA project

---
+++ Labs/SPA/Begin/src/TodoMvc/Controllers/TodosController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using TodoMvc.Models;
+
+namespace TodoMvc.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodosController : Controller
+    {
+        static List<Todo> sampleData = new Todo[] {
+                new Todo { Id = 1, Title = "Item One", Completed = false },
+                new Todo { Id = 2, Title = "Item Two", Completed = true }
+        }.ToList();
+
+        // GET: api/todos
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return sampleData;
+        }
+
+        // GET api/todos/5
+        [HttpGet("{id}")]
+        public Todo Get(int id)
+        {
+            return sampleData.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/todos
+        [HttpPost]
+        public IActionResult Post([FromBody]Todo value)
+        {
+            int maxId = sampleData.Max(t => t.Id);
+            value.Id = maxId + 1;
+            value.Completed = false;
+            sampleData.Add(value);
+
+            var result = new ObjectResult(value);
+            result.StatusCode = (int)HttpStatusCode.Created;
+            return result;
+        }
+
+        // PUT api/todos/5
+        [HttpPut("{id}")]
+        public Todo Put(int id, [FromBody]Todo value)
+        {
+            var item = sampleData.Find(t => t.Id == id);
+            item.Title = value.Title;
+            item.Completed = value.Completed;
+            return item;
+        }
+
+        // DELETE api/todos/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            sampleData.RemoveAll(t => t.Id == id);
+            Context.Response.StatusCode = (int)HttpStatusCode.NoContent;
+        }
+
+        //TODO: Routing not working. Should respond to DELETE sent to /api/todos
+        [HttpDelete("api/[controller]")]
+        public void Archive()
+        {
+            sampleData.RemoveAll(t => t.Completed == true);
+            Context.Response.StatusCode = (int)HttpStatusCode.NoContent;
+        }
+
+        [Route("/api")]
+        public void Api()
+        {
+            Context.Response.StatusCode = (int)HttpStatusCode.OK;
+        }
+    }
+}
---
+++ Labs/SPA/End/src/TodoMvc/gulpfile.js
@@ -0,0 +1,45 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  fs = require("fs");
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+    bower: "./bower_components/",
+    npm: "./node_modules/",
+    lib: "./" + project.webroot + "/lib/"
+};
+
+gulp.task("clean", function (cb) {
+    rimraf(paths.lib, cb);
+});
+
+gulp.task("copy", ["clean"], function () {
+    var bower = {
+        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
+        "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
+        "hammer.js": "hammer.js/hammer*.{js,map}",
+        "jquery": "jquery/jquery*.{js,map}",
+        "jquery-validation": "jquery-validation/jquery.validate.js",
+        "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js",
+        "angular": "angular/angular.js",
+        "angular-route": "angular-route/angular-route.js"
+    }
+
+    for (var destinationDir in bower) {
+        gulp.src(paths.bower + bower[destinationDir])
+          .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+
+    var npm = {
+        "todomvc-app-css": "todomvc-app-css/*.css",
+        "todomvc-common": "todomvc-common/*.{js,css}"
+    }
+
+    for (var destinationDir in npm) {
+        gulp.src(paths.npm + npm[destinationDir])
+          .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+});
